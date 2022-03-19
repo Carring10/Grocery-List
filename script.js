fc5b18c0c2caea0d96076b5d40c0c5bdae723c53
Carrington <hartis.c99@gmail.com>
2022-03-19 18:14:42 -0400

    DOM updates the list with new value

--- script.js
+++ script.js
@@ -9,25 +9,35 @@ function appendListItem() {
     let listItems = JSON.parse(localStorage.getItem("listItems"));
     if (listItems) {
       const itemObject = {
-        groceryItem: itemInput.value,
         isCheckedOff: false,
+        groceryItem: itemInput.value,
       };
+
       listItems.push(itemObject);
       localStorage.setItem("listItems", JSON.stringify(listItems));
+
       for (let i = 0; i < listItems.length; i++) {
-        console.log(listItems[i]);
         const groceryListItem = document.createElement("li");
+        groceryListItem.setAttribute("id", i);
+        const oldListItem = document.getElementById(i.toString());
+        if (oldListItem) listItemsContainer.replaceChild(groceryListItem, oldListItem);
         groceryListItem.innerText = listItems[i].groceryItem;
         listItemsContainer.appendChild(groceryListItem);
       }
     } else {
       const listItemsArray = [];
       const itemObject = {
-        groceryItem: itemInput.value,
         isCheckedOff: false,
+        groceryItem: itemInput.value,
       };
+
       listItemsArray.push(itemObject);
       localStorage.setItem("listItems", JSON.stringify(listItemsArray));
+
+      const groceryListItem = document.createElement("li");
+      groceryListItem.setAttribute("id", "0");
+      groceryListItem.innerText = itemInput.value;
+      listItemsContainer.appendChild(groceryListItem);
     }
 
     itemInput.value = "";

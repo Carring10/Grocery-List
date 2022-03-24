c759ebba21b7cc37a392f5cf9d7fb9542c9a9f14
Carrington <hartis.c99@gmail.com>
2022-03-23 20:00:25 -0400

    list items stay on the page after 1st refresh

--- script.js
+++ script.js
@@ -1,57 +1,56 @@
 const itemInput = document.getElementById("item-input");
 const itemForm = document.getElementById("item-form");
 const listItemsContainer = document.getElementById("list-items-container");
+const listItemsArray = [];
 
-function appendListItem() {
+function addNewListItem() {
   itemForm.addEventListener("submit", function (event) {
     event.preventDefault();
-
-    let listItems = JSON.parse(localStorage.getItem("listItems"));
-    if (listItems) {
-      const itemObject = {
-        isCheckedOff: false,
-        groceryItem: itemInput.value,
-      };
-
-      listItems.push(itemObject);
-      localStorage.setItem("listItems", JSON.stringify(listItems));
-
-      for (let i = 0; i < listItems.length; i++) {
-        const groceryListItem = document.createElement("li");
-        groceryListItem.setAttribute("id", i);
-        const oldListItem = document.getElementById(i.toString());
-        if (oldListItem) listItemsContainer.replaceChild(groceryListItem, oldListItem);
-        groceryListItem.innerText = listItems[i].groceryItem;
-        listItemsContainer.appendChild(groceryListItem);
-      }
-    } else {
-      const listItemsArray = [];
-      const itemObject = {
-        isCheckedOff: false,
-        groceryItem: itemInput.value,
-      };
-
-      listItemsArray.push(itemObject);
-      localStorage.setItem("listItems", JSON.stringify(listItemsArray));
-
-      const groceryListItem = document.createElement("li");
-      groceryListItem.setAttribute("id", "0");
-      groceryListItem.innerText = itemInput.value;
-      listItemsContainer.appendChild(groceryListItem);
+    if (itemInput.value !== "") {
+      appendListItem();
     }
-
     itemInput.value = "";
   });
 }
 
-appendListItem();
+function appendListItem() {
+  const itemObject = {
+    isCheckedOff: false,
+    groceryItem: itemInput.value,
+  };
 
-/* 
- [
-   {
-       value: 'tomatoes',
-       checkedOff: false,
-       category: 'veggies'
-   }
- ]
-*/
+  listItemsArray.push(itemObject);
+  localStorage.setItem("listItems", JSON.stringify(listItemsArray));
+
+  const groceryListItem = document.createElement("li");
+  groceryListItem.setAttribute("id", "0");
+  groceryListItem.innerText = itemInput.value;
+  listItemsContainer.appendChild(groceryListItem);
+}
+
+function getStoredItems() {
+  let listItems = JSON.parse(localStorage.getItem("listItems"));
+  // if (listItems) {
+  //   const itemObject = {
+  //     isCheckedOff: false,
+  //     groceryItem: itemInput.value,
+  //   };
+
+  //   listItems.push(itemObject);
+  //   localStorage.setItem("listItems", JSON.stringify(listItems));
+  if (listItems) {
+    for (let i = 0; i < listItems.length; i++) {
+      const groceryListItem = document.createElement("li");
+      groceryListItem.setAttribute("id", i);
+
+      const oldListItem = document.getElementById(i.toString());
+      if (oldListItem) listItemsContainer.replaceChild(groceryListItem, oldListItem);
+
+      groceryListItem.innerText = listItems[i].groceryItem;
+      listItemsContainer.appendChild(groceryListItem);
+    }
+  }
+}
+
+getStoredItems();
+addNewListItem();
